bdd28cdd9895d049bb56a1fadeb23e03835b2245
tahlil <tahlil@ieee.org>
2021-07-04 18:16:34 +0600

    stop

--- main.py
+++ main.py
@@ -586,12 +586,12 @@ def main():
     # store_addresses()    
     # print(unique_addresses)
     # print(len(unique_addresses))
-    
-    from_block, to_block = 3410001, celo_mainnet_latest_block
-    print(celo_mainnet_latest_block)
-    # from_block, to_block = celo_mainnet_latest_block-100000, celo_mainnet_latest_block
-    user_activities = get_user_activity(from_block, to_block)  
-    call_apis_for_useractivity_data(user_activities)
+    pass
+    # from_block, to_block = 3410001, celo_mainnet_latest_block
+    # print(celo_mainnet_latest_block)
+    # # from_block, to_block = celo_mainnet_latest_block-100000, celo_mainnet_latest_block
+    # user_activities = get_user_activity(from_block, to_block)  
+    # call_apis_for_useractivity_data(user_activities)
     
     print(celo_mainnet_latest_block)
     print("Finished...")
